5a4754a2461cb24dbfb5a3901ee59e1c038089c1
infonalysis <urban.martin@ucdenver.edu>
2014-05-11 00:18:27 -0600

    fix background and add comments for plot1

--- plot1.R
+++ plot1.R
@@ -1,4 +1,13 @@
-mydates <- as.Date(c("2007-02-01", "2007-02-02"))
-power <- read.table("household_power_consumption.txt", sep=";", header=TRUE)[as.Date(read.table("household_power_consumption.txt", sep=";", header=TRUE)$Date,  "%d/%m/%Y") %in% mydates,]
-hist(as.numeric(as.character(power$Global_active_power[power$Global_active_power!="?"])), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+##  plot1.R
+##  Create a frequency histogram of the global active power in kilowatts
 
+##  set up data
+mydates <- as.Date(c("2007-02-01", "2007-02-02"))  # define dates for which to graph measurements
+power <- read.table("household_power_consumption.txt", sep=";", header=TRUE)[as.Date(read.table("household_power_consumption.txt", sep=";", header=TRUE)$Date,  "%d/%m/%Y") %in% mydates,]
+
+##  draw graph and save to png
+
+par(bg=NA)  # set background to transparent
+hist(as.numeric(as.character(power$Global_active_power[power$Global_active_power!="?"])), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")  # draw histogram
+dev.copy(png, file="plot1.png", width=480, height=480)  # save to file 
+dev.off()  # and close device
