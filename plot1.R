2c0bf583430b190a7c7e6b87bc8c2361940f28fe
infonalysis <urban.martin@ucdenver.edu>
2014-05-10 23:08:50 -0600

    added plot1 labels

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
 mydates <- as.Date(c("2007-02-01", "2007-02-02"))
 power <- read.table("household_power_consumption.txt", sep=";", header=TRUE)[as.Date(read.table("household_power_consumption.txt", sep=";", header=TRUE)$Date,  "%d/%m/%Y") %in% mydates,]
-hist(as.numeric(power$Global_active_power[power$Global_active_power!="?"]), col="red")
+hist(as.numeric(power$Global_active_power[power$Global_active_power!="?"]), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
